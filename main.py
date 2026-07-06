12f68dd5061cc4db0ae4ef413bdb028658dcfc83
Charles Ndung'u <ndunguwainaina359@gmail.com>
2026-07-06 21:30:57 +0300

    Prepare for Vercel deployment

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-from fastapi import FastAPI
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello from RAG Project!"}
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/test")
-async def test():
-    return {"message": "Test endpoint working!"}
+def index():
+    return [
+        {   
+            "Name": "Charles",
+            "Course": "Data Science"
+        },
+        {   
+            "Name": "John",
+            "Course": "Data Science"
+        }
+    ]
 
-# Vercel expects 'app' to be the FastAPI instance
-# If you have if __name__ == "__main__", keep it but Vercel ignores it
+@app.get("/home", response_class=HTMLResponse)
+def home_page(request: Request):
+    return templates.TemplateResponse(
+        name="index.html",
+        context={"request": request, "message": "By Charles"}
+    )
+    
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=5000)
+
+
     
 
